c7f5f7b3c02cc5f6d640b075ae159bdc98daa25a
Bon Daggao <bsdaggao2@student.apc.edu.ph>
2023-12-15 23:06:22 +0800

    testing some new code

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,52 @@
 <template>
   <div>
-    This is the home page
+    <body>
+      <div class="menu">
+        <main>
+          <div class="logo-container">
+            <img src="/5Logo.png" alt="Logo" class="logo" />
+          </div>
+          <div class="info">
+            <h1>CAMPER CAFE</h1>
+            <p>Est. 2020</p>
+            <section>
+              <h2>Coffee</h2>
+            </section>
+          </div>
+        </main>
+      </div>
+    </body>
+    <p>Welcome to our website!</p>
   </div>
 </template>
+
+<style>
+ .menu {
+  text-align: center;
+  border-radius: 25px;
+  height: 20em;
+  width: 90%;
+  background-color: #ffaa2b;
+  margin: auto;
+  margin-top: 30px;
+}
+
+.logo-container {
+  margin-bottom: 20px; /* Adjust the margin as needed */
+}
+
+.logo {
+  width: 100px; /* Set the width of your logo */
+  height: auto; /* Maintain aspect ratio */
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+p {
+  text-align: center;
+}
+</style>
